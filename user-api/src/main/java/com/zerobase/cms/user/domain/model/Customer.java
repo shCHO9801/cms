eb87155db7f849898fbb570ee82b076a66e60b13
shCHO9801 <csh980116@naver.com>
2024-11-21 00:11:54 +0900

    feat : [002-SIGN-UP] 커스터머(고객)에 대한 Signup Service 구현

---
+++ user-api/src/main/java/com/zerobase/cms/user/domain/model/Customer.java
@@ -0,0 +1,46 @@
+package com.zerobase.cms.user.domain.model;
+
+import com.zerobase.cms.user.domain.SignUpForm;
+import lombok.*;
+import lombok.NoArgsConstructor;
+import org.hibernate.envers.AuditOverride;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@AuditOverride(forClass = BaseEntity.class)
+public class Customer extends BaseEntity {
+
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String email;
+    private String name;
+    private String password;
+    private String phone;
+    private LocalDate birth;
+
+    private LocalDateTime verifyExpiredAt;
+    private String verificationCode;
+    private boolean verify;
+
+    public static Customer from (SignUpForm form) {
+        return Customer.builder()
+                .email(form.getEmail())
+                .name(form.getName())
+                .password(form.getPassword())
+                .phone(form.getPhone())
+                .birth(form.getBirth())
+                .verify(false)
+                .build();
+    }
+}
