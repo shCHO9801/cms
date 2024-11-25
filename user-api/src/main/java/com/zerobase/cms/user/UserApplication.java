a08b55c89c6310c0f5c9641bc9d695fc44894f4e
shCHO9801 <csh980116@naver.com>
2024-11-25 13:32:42 +0900

    feat : [004-LOGIN_CUSTOMER] 커스터머에 대한 로그인 구현
    JWT의 내부 암호화
    /customer/* 에 대한 token filter 적용

--- user-api/src/main/java/com/zerobase/cms/user/UserApplication.java
+++ user-api/src/main/java/com/zerobase/cms/user/UserApplication.java
@@ -3,14 +3,16 @@ package com.zerobase.cms.user;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@ServletComponentScan
 @EnableFeignClients
 @SpringBootApplication
-//@EnableJpaAuditing
-//@EnableJpaRepositories
+@EnableJpaAuditing
+@EnableJpaRepositories
 @RequiredArgsConstructor
 public class UserApplication {
 
---
+++ user-apitmp/src/main/java/com/zerobase/cms/user/application/SignInApplication.java
@@ -0,0 +1,26 @@
+package com.zerobase.cms.user.application;
+
+import com.zerobase.cms.user.domain.SignInForm;
+import com.zerobase.cms.user.domain.model.Customer;
+import com.zerobase.cms.user.exception.CustomException;
+import com.zerobase.cms.user.service.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import static com.zerobase.cms.user.exception.ErrorCode.LOGIN_CHECK_FAIL;
+
+@Service
+@RequiredArgsConstructor
+public class SignInApplication {
+
+    private final CustomerService customerService;
+
+    public String customerLoginToken(SignInForm form) {
+        // 1. 로그인 가능 여부
+        Customer c = customerService.findValidCustomer(form.getEmail(), form.getPassword())
+                .orElseThrow(() -> new CustomException(LOGIN_CHECK_FAIL));
+        // 2. 토큰을 발행하고
+        // 3. 토큰을 response 한다.
+        return null;
+    }
+}
