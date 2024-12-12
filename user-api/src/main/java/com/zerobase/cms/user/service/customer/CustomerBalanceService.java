0d01f31185f2d32a2f8cfd4911173b5c71d45b62
shCHO9801 <csh980116@naver.com>
2024-12-13 00:36:24 +0900

    feat : [006-CUSTOMER-BALANCE] 고객에 대한 잔액 조회 구현.

---
+++ user-api/src/main/java/com/zerobase/cms/user/service/customer/CustomerBalanceService.java
@@ -0,0 +1,45 @@
+package com.zerobase.cms.user.service.customer;
+
+import com.zerobase.cms.user.domain.customer.ChangeBalanceForm;
+import com.zerobase.cms.user.domain.model.CustomerBalanceHistory;
+import com.zerobase.cms.user.domain.repository.CustomerBalanceHistoryRepository;
+import com.zerobase.cms.user.domain.repository.CustomerRepository;
+import com.zerobase.cms.user.exception.CustomException;
+import com.zerobase.cms.user.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerBalanceService {
+
+    private final CustomerBalanceHistoryRepository customerBalanceHistoryRepository;
+    private final CustomerRepository customerRepository;
+
+    @Transactional(noRollbackFor = {CustomException.class})
+    public CustomerBalanceHistory changeBalance(Long customerId, ChangeBalanceForm form) {
+        CustomerBalanceHistory customerBalanceHistory =
+                customerBalanceHistoryRepository.findFirstByCustomer_IdOrderByIdDesc(customerId)
+                        .orElse(CustomerBalanceHistory.builder()
+                                .changeMoney(0)
+                                .currentMoney(0)
+                                .customer(customerRepository.findById(customerId)
+                                        .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_USER)))
+                                .build());
+        if(customerBalanceHistory.getCurrentMoney() + form.getMoney() < 0) {
+            throw new CustomException(ErrorCode.NOT_ENOUGH_BALANCE);
+        }
+
+        customerBalanceHistory = CustomerBalanceHistory.builder()
+                .changeMoney(form.getMoney())
+                .currentMoney(customerBalanceHistory.getCurrentMoney() + form.getMoney())
+                .description(form.getMessage())
+                .fromMessage(form.getFrom())
+                .customer(customerBalanceHistory.getCustomer())
+                .build();
+        customerBalanceHistory.getCustomer().setBalance(customerBalanceHistory.getChangeMoney());
+
+        return customerBalanceHistoryRepository.save(customerBalanceHistory);
+    }
+}
